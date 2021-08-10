85ea8e5df55aadeb6600f91e65c8e36c2bdcedb8
manaarr <manaarrzakaria@gmail.com>
2021-08-11 00:09:48 +0200

    upload

--- RazorPagesDemo/Pages/Forms/UploadFile.cshtml.cs
+++ RazorPagesDemo/Pages/Forms/UploadFile.cshtml.cs
@@ -11,22 +11,29 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace RazorPagesDemo.Pages.Forms
 {
     public class UploadFileModel : PageModel
-    {
-
+    { 
         private IWebHostEnvironment _environment;
+        public const string MessageKey = nameof(MessageKey);
         public UploadFileModel(IWebHostEnvironment environment)
         {
             _environment = environment;
         }
         [BindProperty]
         public IFormFile Upload { get; set; }
-        public async Task OnPostAsync()
+        public async Task<IActionResult> OnPostAsync()
         {
-            var file = Path.Combine(_environment.ContentRootPath, "uploads", Upload.FileName);
+            if (Upload == null || Upload.Length == 0)
+            {
+                TempData[MessageKey] = "Upload failed!";
+                return RedirectToAction(Request.Path);
+            }
+            var file = Path.Combine(_environment.WebRootPath, "uploads", Upload.FileName);
             using (var fileStream = new FileStream(file, FileMode.Create))
             {
                 await Upload.CopyToAsync(fileStream);
             }
+            TempData[MessageKey] = "Upload successful!";
+            return RedirectToAction(Request.Path);
         }
         public void OnGet()
         {
